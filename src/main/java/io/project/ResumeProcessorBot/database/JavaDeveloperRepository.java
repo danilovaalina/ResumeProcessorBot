197e5cfaa728ae1ad2a0f42639f8af861d0c98de
Danilova Alina <ialinerpromax@gmail.com>
2023-02-11 18:56:05 +0300

    Add database to telegram bot

---
+++ src/main/java/io/project/ResumeProcessorBot/database/JavaDeveloperRepository.java
@@ -0,0 +1,7 @@
+package io.project.ResumeProcessorBot.database;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface JavaDeveloperRepository extends CrudRepository<JavaDeveloper, Integer> {
+
+}
