1e52291971273f0913762e7e6009a0235a8f62de
Danilova Alina <ialinerpromax@gmail.com>
2023-05-13 23:07:01 +0300

    Change the logic of VacancyService. HeadHunter API Integration

--- src/main/java/io/project/ResumeProcessorBot/service/VacancyService.java
+++ src/main/java/io/project/ResumeProcessorBot/service/VacancyService.java
@@ -1,48 +1,92 @@
 package io.project.ResumeProcessorBot.service;
 
-import io.project.ResumeProcessorBot.model.vacancy.Vacancy;
-import lombok.AllArgsConstructor;
-import okhttp3.HttpUrl;
+import io.project.ResumeProcessorBot.entity.Vacancy;
+import io.project.ResumeProcessorBot.repository.VacancyRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.Objects;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+@Slf4j
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class VacancyService {
-    private final String USER_AGENT = "Mozilla/5.0";
+    VacancyRepository vacancyRepository;
+    Vacancy vacancy;
 
-    @Value("${app.default-page-size}")
-    private int defaultPageSize;
-
-    @Value("${url.hh-ru.vacancies}")
-    private String urlVacancies;
-
-    private RestTemplate template;
+    @Value("${hh-ru.api-url}")
+    String URL;
 
 
-
-
-    private String getUrl(Vacancy vacancy, Integer page) {
-        page = page == null ? 0 : page;
-        HttpUrl.Builder urlBuilder = Objects.requireNonNull(HttpUrl.parse(urlVacancies)).newBuilder();
-
-        urlBuilder.addQueryParameter("page", String.valueOf(page));
-        urlBuilder.addQueryParameter("per_page", String.valueOf(defaultPageSize));
-
-        if (vacancy != null) {
-            urlBuilder.addQueryParameter("experience", vacancy.getExperience());
-            urlBuilder.addQueryParameter("employment", vacancy.getEmployment());
-            urlBuilder.addQueryParameter("schedule", vacancy.getSchedule());
-            urlBuilder.addQueryParameter("text", vacancy.getText());
-            urlBuilder.addQueryParameter("only_with_salary", String.valueOf(vacancy.getOnlyWithSalary()));
+    public List<Vacancy> getVacancies(String vacancyName) {
+        List<Vacancy> vacancies = vacancyRepository.searchByName(vacancyName);
+        if (vacancies.isEmpty()) {
+            addVacancy(vacancyName);
+            vacancies = vacancyRepository.searchByName(vacancyName);
         }
-
-        return urlBuilder.toString();
+        return vacancies;
     }
 
+    private void addVacancy(String vacancyName) {
+        List<Vacancy> vacancies = new ArrayList<>();
+        String result = getResult(vacancyName);
+        JSONObject jsonObject = new JSONObject(result);
+        JSONArray items = jsonObject.getJSONArray("items");
+        for(int i = 0; i < items.length(); i++) {
+            JSONObject jObj = items.getJSONObject(i);
+            if (jObj.has("name")) {
+                vacancy.setName(jObj.getString("name"));
+            }
+            if (jObj.has("schedule") && !jObj.isNull("schedule")) {
+                vacancy.setSchedule(jObj.getString("schedule"));
+            }
+            JSONObject snippet = jObj.getJSONObject("snippet");
+            if (snippet.has("requirement")) {
+                vacancy.setRequirement(snippet.getString("requirement").
+                        replaceAll("<highlighttext>", "").replaceAll("</highlighttext>", ""));
+            }
+            JSONObject area = jObj.getJSONObject("area");
+            if (area.has("name")) {
+                vacancy.setArea(area.getString("name"));
+            }
+            vacancies.add(vacancy);
+        }
+        vacancyRepository.saveAll(vacancies);
+    }
 
-
+    private @NotNull String getResult(String vacancyName) {
+        StringBuilder result = new StringBuilder();
+        try {
+            String paramValue = URLEncoder.encode(vacancyName, StandardCharsets.UTF_8);
+            URL url = new URL(MessageFormat.format("{0}vacancies?text={1}", URL, paramValue));
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            String USER_AGENT = "Mozilla/5.0";
+            connection.setRequestProperty("User-Agent", USER_AGENT);
+            Scanner scanner = new Scanner((InputStream) connection.getContent());
+            while(scanner.hasNextLine()) {
+                result.append(scanner.nextLine());
+            }
+        } catch (IOException e) {
+            log.error("Неверный URL-адрес"  +
+                    "\nСтрока не может быть проанализирована или без надлежащего протокола");
+        }
+        return result.toString();
+    }
 }
